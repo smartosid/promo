f8e3c49acd2140bf7ce4a4f9da6a7c8ab01e2598
Siddarth Vinnakota <62463134+smartosid@users.noreply.github.com>
2024-03-28 14:14:10 +0530

    Update Timer.js
    added buttons long short breaks

--- src/Timer.js
+++ src/Timer.js
@@ -4,17 +4,18 @@ import PlayButton from "./PlayButton";
 import PauseButton from "./PauseButton";
 import ResetButton from "./reset";
 import SettingsButton from "./SettingsButton";
-import { useContext, useState, useEffect, useRef } from "react";
+import { useContext, useState, useEffect, useRef, useCallback } from "react";
 import SettingsContext from "./SettingsContext";
 
 const red = '#7469B6';
 const green = '#4aec8c';
+const blue = '#6495ED';
 
 function Timer() {
   const settingsInfo = useContext(SettingsContext);
 
   const [isPaused, setIsPaused] = useState(true);
-  const [mode, setMode] = useState('work');
+  const [mode, setMode] = useState('promodoro');
   const [secondsLeft, setSecondsLeft] = useState(0);
   const [completedPomodoros, setCompletedPomodoros] = useState(0);
 
@@ -22,36 +23,54 @@ function Timer() {
   const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
 
+  const getInitialSeconds = useCallback((mode) => {
+    switch (mode) {
+      case 'promodoro':
+        return settingsInfo.workMinutes * 60;
+      case 'shortBreak':
+        return 15 * 60;
+      case 'longBreak':
+        return 30 * 60;
+      default:
+        return settingsInfo.workMinutes * 60;
+    }
+  }, [settingsInfo]);
+
   function tick() {
     secondsLeftRef.current--;
     setSecondsLeft(secondsLeftRef.current);
   }
 
   function handleReset() {
-    const initialSeconds = mode === 'work' ? settingsInfo.workMinutes * 60 : settingsInfo.breakMinutes * 60;
+    const initialSeconds = getInitialSeconds(modeRef.current);
     setSecondsLeft(initialSeconds);
     secondsLeftRef.current = initialSeconds;
   }
 
   useEffect(() => {
-
     function switchMode() {
-      const nextMode = modeRef.current === 'work' ? 'break' : 'work';
-      const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
+      let nextMode;
+      if (modeRef.current === 'promodoro') {
+        nextMode = 'shortBreak';
+      } else if (modeRef.current === 'shortBreak') {
+        nextMode = 'longBreak';
+      } else {
+        nextMode = 'promodoro';
+        setCompletedPomodoros(prevCount => prevCount + 1);
+      }
+
+      const nextSeconds = getInitialSeconds(nextMode);
 
       setMode(nextMode);
       modeRef.current = nextMode;
 
       setSecondsLeft(nextSeconds);
       secondsLeftRef.current = nextSeconds;
-
-      if (nextMode === 'work') {
-        setCompletedPomodoros(prevCount => prevCount + 1);
-      }
     }
 
-    secondsLeftRef.current = settingsInfo.workMinutes * 60;
-    setSecondsLeft(secondsLeftRef.current);
+    const initialSeconds = getInitialSeconds(mode);
+    secondsLeftRef.current = initialSeconds;
+    setSecondsLeft(initialSeconds);
 
     const interval = setInterval(() => {
       if (isPausedRef.current) {
@@ -65,40 +84,95 @@ function Timer() {
     }, 1000);
 
     return () => clearInterval(interval);
-  }, [settingsInfo]);
+  }, [settingsInfo, getInitialSeconds, mode]);
 
-  const totalSeconds = mode === 'work'
-    ? settingsInfo.workMinutes * 60
-    : settingsInfo.breakMinutes * 60;
+  const totalSeconds = getInitialSeconds(mode);
   const percentage = Math.round(secondsLeft / totalSeconds * 100);
 
   const minutes = Math.floor(secondsLeft / 60);
   let seconds = secondsLeft % 60;
-  if(seconds < 10) seconds = '0'+seconds;
+  if (seconds < 10) seconds = '0' + seconds;
 
   return (
     <div style={{ width: '500px', borderRadius: '20px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', padding: '20px', marginBottom: '20px', backgroundColor: '#CEDDDA', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', marginBottom: '20px' }}>
+                  
+          <button
+            style={{
+              backgroundColor: mode === 'promodoro' ? red : 'white',
+              color: mode === 'promodoro' ? 'white' : 'black',
+              padding: '10px 20px',
+              borderRadius: '5px',
+              marginRight: '10px',
+            }}
+            onClick={() => {
+              setMode('promodoro');
+              modeRef.current = 'promodoro';
+              const initialSeconds = getInitialSeconds('promodoro');
+              setSecondsLeft(initialSeconds);
+              secondsLeftRef.current = initialSeconds;
+            }}
+          >
+            Promodoro
+          </button>
+          <button
+            style={{
+              backgroundColor: mode === 'shortBreak' ? green : 'white',
+              color: mode === 'shortBreak' ? 'white' : 'black',
+              padding: '10px 20px',
+              borderRadius: '5px',
+              marginRight: '10px',
+            }}
+            onClick={() => {
+              setMode('shortBreak');
+              modeRef.current = 'shortBreak';
+              const initialSeconds = getInitialSeconds('shortBreak');
+              setSecondsLeft(initialSeconds);
+              secondsLeftRef.current = initialSeconds;
+            }}
+          >
+            Short Break
+          </button>
+          <button
+            style={{
+              backgroundColor: mode === 'longBreak' ? blue : 'white',
+              color: mode === 'longBreak' ? 'white' : 'black',
+              padding: '10px 20px',
+              borderRadius: '5px',
+            }}
+            onClick={() => {
+              setMode('longBreak');
+              modeRef.current = 'longBreak';
+              const initialSeconds = getInitialSeconds('longBreak');
+              setSecondsLeft(initialSeconds);
+              secondsLeftRef.current = initialSeconds;
+            }}
+          >
+            Long Break
+          </button>
+        </div>
         <div style={{ width: '200px', height: '200px', position: 'relative' }}>
           <CircularProgressbar
             value={percentage}
             text={minutes + ':' + seconds}
             styles={buildStyles({
-              textColor:'#EEEEEE',
-              pathColor:mode === 'work' ? red : green,
-              tailColor:'rgba(255,255,255,.2)',
-            })} />
+              textColor: '#EEEEEE',
+              pathColor: mode === 'promodoro' ? red : mode === 'shortBreak' ? green : blue,
+              tailColor: 'rgba(255,255,255,.2)',
+            })}
+          />
         </div>
-        <div style={{marginTop:'20px', display: 'flex', alignItems: 'center' }}>
+        <div style={{ marginTop: '20px', display: 'flex', alignItems: 'center' }}>
           {isPaused
             ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} />
             : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />}
-          <ResetButton onClick={handleReset} style={{marginLeft: '10px',backgroundColor:'#6962AD'}} />
+          <ResetButton onClick={handleReset} style={{ marginLeft: '10px', backgroundColor: '#6962AD' }} />
         </div>
-        <div style={{marginTop:'10px'}}>
+        <div style={{ marginTop: '10px' }}>
           <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
         </div>
-        <div style={{marginTop:'5px',fontSize:"25px"}}>
+        <div style={{ marginTop: '5px', fontSize: "25px" }}>
           <p>Pomodoros: {completedPomodoros}</p>
         </div>
       </div>
