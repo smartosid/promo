74c2d3c00e4b0bde62d3e5d152f9b94c7b76bd0c
Siddarth Vinnakota <62463134+smartosid@users.noreply.github.com>
2024-03-29 10:52:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
 import Timer from "./Timer";
+
 import Settings from "./Settings";
 import { useState } from "react";
 import SettingsContext from "./SettingsContext";
-import TodoList from './TodoList'; // Import the TodoList component
+import TodoList from './TodoList'; 
 
 function App() {
   const [showSettings, setShowSettings] = useState(false);
@@ -12,10 +13,12 @@ function App() {
   const [breakMinutes, setBreakMinutes] = useState(15);
 
   return (
-    <div className="flex-container">
-       <main>
+
+    <main>
+       <div class="container">
        <div className='try'>
-      <header> <h1>promofocus</h1></header>
+      
+      <header> <h1>Promofocus</h1></header>
    
       
       {/* Render Timer or Settings based on showSettings state */}
@@ -27,9 +30,10 @@ function App() {
     <div className="todo-section">
       <TodoList />
     </div>
-  </main>
-      
     </div>
+  </main>
+  
+   
   );
 }
 
